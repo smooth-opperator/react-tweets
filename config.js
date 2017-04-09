2801383d76e211e200ae49f5091cd6b51eff6b98
opperator <julie.e.oppermann@gmail.com>
2017-04-09 14:13:43 +0200

    added repository information

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  twitter: {
-    consumer_key: 'MY_CONSUMER_KEY',
-    consumer_secret: 'MY_CONSUMER_SECRET',
-    access_token_key: 'MY_ACCESS_TOKEN_KEY',
-    access_token_secret: 'MY_ACCESS_TOKEN_SECRET'
-  }
-}
